81864a87a0f8b6af893a086a7871a1b5d097df3f
oleg425top <oleg59152@gmail.com>
2025-06-09 17:11:56 +0500

    добавил с основные url's static

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-]
+                  path('admin/', admin.site.urls),
+                  path('users/', include('users.urls', namespace='users')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
