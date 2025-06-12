0251489703de7e05fa19263d07611d85252e42de
oleg425top <oleg59152@gmail.com>
2025-06-12 14:53:50 +0500

    Исправил директорию static в базовых настройках

--- config/settings.py
+++ config/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = 'static/assets/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/assets/',
 )
 
 MEDIA_URL = '/media/'
