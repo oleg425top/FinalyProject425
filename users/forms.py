11cb810d03b09ba814fa0fc1d263dc71a5e51f03
oleg425top <oleg59152@gmail.com>
2025-06-12 14:50:48 +0500

    Добавил форму регистрации для Users

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from users.models import User
+
+# from users.validator import validate_password
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth import password_validation
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
