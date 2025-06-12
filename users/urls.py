3afba875213a05693937ba167703e403e2c326a5
oleg425top <oleg59152@gmail.com>
2025-06-12 14:54:30 +0500

    Добавил url для регистрации

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import index_view
+from users.views import index_view, UserRegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('users/register/', UserRegisterView.as_view(), name='register'),
 ]
