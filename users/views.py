d26f9ce52889e2635e11e8cab9b18b1c730c324b
oleg425top <oleg59152@gmail.com>
2025-06-13 16:52:26 +0500

    Добавил необходимые импорты

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from users.models import User
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserLoginForm, UserForm
 
 
 def index_view(request):
@@ -17,3 +18,33 @@ class UserRegisterView(CreateView):
     extra_context = {
         'title': 'Создать аккаунт'
     }
+
+
+class UserLogoutView(LogoutView):
+    template_name = 'users/logout.html'
+    extra_context = {
+        'title': 'Выход из аккаунта'
+    }
+
+
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+    success_url = ''
+    extra_context = {
+        'title': 'Вход в аккаунт'
+    }
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/profile.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data()
+        context_data['title'] = f'Ваш профиль {self.get_object()}'
+        return context_data
