5caf2986053875c770d89282ba740827c7578d82
oleg425top <oleg59152@gmail.com>
2025-06-13 16:51:58 +0500

    Добавил новые url's для пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import index_view, UserRegisterView
+from users.views import index_view, UserRegisterView, UserLogoutView, UserLoginView, UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('users/register/', UserRegisterView.as_view(), name='register'),
+    path('users/logout/', UserLogoutView.as_view(), name='logout'),
+    path('users/login/', UserLoginView.as_view(), name='login'),
+    path('users/profile/', UserProfileView.as_view(), name='profile'),
 ]
