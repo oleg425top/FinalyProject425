5efed34208d77b59f303c94871dd6f0c0e442c37
oleg425top <oleg59152@gmail.com>
2025-06-16 14:51:21 +0500

    Добавил url's для обновления профиля пользователя и изменения пароля

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import index_view, UserRegisterView, UserLogoutView, UserLoginView, UserProfileView
+from users.views import index_view, UserRegisterView, UserLogoutView, UserLoginView, UserProfileView, UserUpdateView, \
+    UserPasswordChangeView
 
 app_name = UsersConfig.name
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('users/logout/', UserLogoutView.as_view(), name='logout'),
     path('users/login/', UserLoginView.as_view(), name='login'),
     path('users/profile/', UserProfileView.as_view(), name='profile'),
+    path('users/update/', UserUpdateView.as_view(), name='update'),
+    path('change_password/', UserPasswordChangeView.as_view(), name='change_password'),
 ]
