05655e255f774895d265c25c1cbeb2963e78dc74
oleg425top <oleg59152@gmail.com>
2025-06-16 14:49:11 +0500

    Добавил формы: UserUpdateForm UserChangePasswordForm

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.password_validation import validate_password
+
 from users.models import User
 
 # from users.validator import validate_password
@@ -6,11 +8,13 @@ from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, Auth
 from django.core.exceptions import ValidationError
 from django.contrib.auth import password_validation
 
+
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
 
 class UserForm(StyleFormMixin, forms.ModelForm):
     class Meta:
@@ -28,3 +32,20 @@ class UserLoginForm(StyleFormMixin, AuthenticationForm):
     pass
 
 
+class UserUpdateForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'phone',)
+
+class UserChangePasswordForm(StyleFormMixin, PasswordChangeForm):
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        validate_password(password1)
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch'
+            )
+        password_validation.validate_password(password2, self.user)
+        return password2
