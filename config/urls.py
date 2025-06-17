d21538e090caf7a5efd26d747da8b5d583e575be
oleg425top <oleg59152@gmail.com>
2025-06-17 19:04:48 +0500

    Добавил shop.urls в базовые urls

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                 path('admin/', admin.site.urls),
                 path('', include('users.urls', namespace='users')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                path('', include('shop.urls', namespace='shop')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
