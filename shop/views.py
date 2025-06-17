9510462ddeb8e96b352d1c8455cf09f6a7ef07e2
oleg425top <oleg59152@gmail.com>
2025-06-17 19:06:33 +0500

    Добавил BrandListView для shop

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from shop.models import Brand
+
+
+class BrandListView(ListView):
+    model = Brand
+    extra_context = {
+        'title': 'Все наши породы'
+    }
+    template_name = 'shop/brands.html'
+    paginate_by = 3
