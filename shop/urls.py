0e90ad6615f2136ae24c620b725387bf8f49f1f3
oleg425top <oleg59152@gmail.com>
2025-06-18 15:29:16 +0500

    Добавил urls для shop

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page, never_cache
 
-from users.apps import UsersConfig
+from shop.apps import ShopConfig
+from shop.views import BrandListView, BrandCreateView, about_view
 
-
-app_name = UsersConfig.name
+app_name = ShopConfig.name
 
 urlpatterns = [
+    path('about/', about_view, name='about'),
+    path('catalog/', cache_page(1)(BrandListView.as_view()), name='catalog'),
+    path('catalog/brand_create/', BrandCreateView.as_view(), name='brand_create'),
 
 ]
