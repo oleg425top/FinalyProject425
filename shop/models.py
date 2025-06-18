94df9731af55cd033b00cdb7ecd55452b3dd4830
oleg425top <oleg59152@gmail.com>
2025-06-18 15:22:59 +0500

    Изменил читаемое имя на русский язык

--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100, verbose_name='brand_name')
-    description = models.TextField(blank=True, null=True, verbose_name='description')
-    logo = models.ImageField(upload_to='brand_logos/', verbose_name='brand_logo')  # Поле для логотипа
+    name = models.CharField(max_length=100, verbose_name='Название брэнда')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    logo = models.ImageField(upload_to='brand_logos/', verbose_name='Логотип')  # Поле для логотипа
 
     def __str__(self):
         return self.name
