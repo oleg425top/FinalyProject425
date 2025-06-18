b6252df0f255b1308805bd5bde188856dd766642
oleg425top <oleg59152@gmail.com>
2025-06-18 15:23:48 +0500

    Изменил STATIC_URL в главных настройках проекта

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/assets/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static/assets/',
 )
