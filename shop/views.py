ee5baaf2ff7535e9023312755b6a9e43c3619b87
oleg425top <oleg59152@gmail.com>
2025-06-18 15:31:33 +0500

    Изменил title для BrandListView Добавил BrandCreateView

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,44 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
+from shop.forms import BrandForms
 from shop.models import Brand
+from users.models import UserRolls
+
+
+def about_view(request):
+    context = {
+        'title': 'Электроинструмент - о нас',
+    }
+
+    return render(request, 'shop/about.html', context=context)
 
 
 class BrandListView(ListView):
     model = Brand
     extra_context = {
-        'title': 'Все наши породы'
+        'title': 'Все наши брэнды'
     }
-    template_name = 'shop/brands.html'
+    template_name = 'shop/brand_list.html'
     paginate_by = 3
+
+
+class BrandCreateView(LoginRequiredMixin, CreateView):
+    model = Brand
+    form_class = BrandForms
+    template_name = 'shop/brand_create.html'
+    extra_context = {
+        'title': 'Добавить брэнд'
+    }
+    success_url = reverse_lazy('shop:catalog')
+
+    def form_valid(self, form):
+        if self.request.user.role == UserRolls.USER:
+            raise PermissionDenied()
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
