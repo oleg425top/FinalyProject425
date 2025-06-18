2495431a94387a614712ea411ef269e3d9700c95
oleg425top <oleg59152@gmail.com>
2025-06-18 15:24:42 +0500

    Изменил базовые Url's проекта для приложения shop

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                 path('admin/', admin.site.urls),
                 path('', include('users.urls', namespace='users')),
-                path('', include('shop.urls', namespace='shop')),
+                path('shop/', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
