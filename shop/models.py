eae9e0ac2626c5e1311f18b626cf89cea14cf2e2
oleg425top <oleg59152@gmail.com>
2025-06-19 16:15:22 +0500

    Добавил поле slug в модель Brand Снёс и восстановил базу данных Создал и применил миграции Создал примеры брэндов с логотипами

--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название брэнда')
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название брэнда')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    logo = models.ImageField(upload_to='brand_logos/', verbose_name='Логотип')  # Поле для логотипа
+    logo = models.ImageField(upload_to='brand_logos/', verbose_name='Логотип')
+    slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
