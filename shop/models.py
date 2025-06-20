a69be931edd7742eef1d59776d8b0bbe12643416
oleg425top <oleg59152@gmail.com>
2025-06-20 13:34:41 +0500

    Изменил verbose_name

--- shop/models.py
+++ shop/models.py
@@ -15,14 +15,14 @@ class Brand(models.Model):
         verbose_name_plural = 'Брэнды'
 
 class Tool(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Название брэнда')
+    name = models.CharField(max_length=100, unique=True, verbose_name='Модель')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
     image = models.ImageField(upload_to='tool_image/', blank=True, null=True, verbose_name='Фото инструмента')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name='Категория')
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name='Брэнд')
 
     def __str__(self):
         return f'({self.name}.Количество: {self.quantity}'
