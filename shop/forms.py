c329d88e6b9beb83bc5f8853da0214b05ba399b3
oleg425top <oleg59152@gmail.com>
2025-06-20 17:25:01 +0500

    Добавил форму для Tool

--- shop/forms.py
+++ shop/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
-from shop.models import Brand
+from shop.models import Brand, Tool
 from users.forms import StyleFormMixin
 
 
 class BrandForms(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Brand
+        fields = '__all__'
+
+class ToolForms(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Tool
         fields = '__all__'
