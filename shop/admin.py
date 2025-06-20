98e281c1b5d9872dbaf99ed55338493fb1b3a85e
oleg425top <oleg59152@gmail.com>
2025-06-20 12:11:54 +0500

    Добавил модель Tool и зарегестрировал ее в админке Создал и применил миграции для Tool

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from shop.models import Brand
+from shop.models import Brand, Tool
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'logo')
+    list_filter = ('name',)
+
+
+@admin.register(Tool)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'price')
     list_filter = ('name',)
--- shop/models.py
+++ shop/models.py
@@ -13,3 +13,17 @@ class Brand(models.Model):
     class Meta:
         verbose_name = 'Брэнд'
         verbose_name_plural = 'Брэнды'
+
+class Tool(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название брэнда')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to='tool_image/', blank=True, null=True, verbose_name='Фото инструмента')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name='Категория')
+
+    class Meta:
+        verbose_name = 'Инструмент'
+        verbose_name_plural = 'Инструменты'
