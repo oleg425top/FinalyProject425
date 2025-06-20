147347e3a4090034cbb9b1c79e257c0ed2801649
oleg425top <oleg59152@gmail.com>
2025-06-20 19:54:29 +0500

    Добавил шаблоны для создания и удаления Tool. Прописал соответствующие url's

--- shop/urls.py
+++ shop/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from django.views.decorators.cache import cache_page, never_cache
 
 from shop.apps import ShopConfig
-from shop.views import BrandListView, BrandCreateView, about_view, ToolListView
+from shop.views import BrandListView, BrandCreateView, about_view, ToolListView, ToolCreateView, ToolDeleteView
 
 app_name = ShopConfig.name
 
 urlpatterns = [
     path('about/', about_view, name='about'),
     path('catalog/brands/', cache_page(1)(BrandListView.as_view()), name='catalog'),
+
+    path('catalog/brands/create/', BrandCreateView.as_view(), name='brand_create'),
     path('catalog/tools/', cache_page(1)(ToolListView.as_view()), name='tools'),
-    path('catalog/brand_create/', BrandCreateView.as_view(), name='brand_create'),
+    path('catalog/tools/create', cache_page(1)(ToolCreateView.as_view()), name='tool_create'),
+    path('catalog/tools/delete/<int:pk>/', cache_page(1)(ToolDeleteView.as_view()), name='tool_delete'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView
 
-from shop.forms import BrandForms
+from shop.forms import BrandForms, ToolForms
 from shop.models import Brand, Tool
 from users.models import UserRolls
 
@@ -50,3 +50,34 @@ class ToolListView(ListView):
     }
     template_name = 'shop/tool_list.html'
     paginate_by = 3
+
+class ToolCreateView(LoginRequiredMixin, CreateView):
+    model = Tool
+    form_class = ToolForms
+    template_name = 'shop/tool_create.html'
+    extra_context = {
+        'title': 'Добавить Инструмент'
+    }
+    success_url = reverse_lazy('shop:tools')
+
+    def form_valid(self, form):
+        if self.request.user.role != UserRolls.ADMIN:
+            raise PermissionDenied()
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class ToolDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Tool
+    template_name = 'shop/tool_delete.html'
+    success_url = reverse_lazy('shop:tools')
+    permission_required = 'shop.delete_tool'
+    permission_denied_message = 'У вас нет нужных прав'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        object_ = self.get_object()
+        context_data['title'] = f'Удалить инструмент {object_}'
+        return context_data
