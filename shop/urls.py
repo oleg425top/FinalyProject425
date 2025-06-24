3542c36059606dd038d799648aba68af5f7d1734
oleg425top <oleg59152@gmail.com>
2025-06-24 12:41:52 +0500

    Подключил слаги для html адресов и url's

--- shop/urls.py
+++ shop/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('catalog/brands/create/', BrandCreateView.as_view(), name='brand_create'),
     path('catalog/tools/', cache_page(1)(ToolListView.as_view()), name='tools'),
     path('catalog/tools/create', cache_page(1)(ToolCreateView.as_view()), name='tool_create'),
-    path('catalog/tools/delete/<int:pk>/', cache_page(1)(ToolDeleteView.as_view()), name='tool_delete'),
-    path('catalog/tools/update/<int:pk>/', cache_page(1)(ToolUpdateView.as_view()), name='tool_update'),
+    path('catalog/tools/delete/<slug:slug>/', cache_page(1)(ToolDeleteView.as_view()), name='tool_delete'),
+    path('catalog/tools/update/<slug:slug>/', cache_page(1)(ToolUpdateView.as_view()), name='tool_update'),
     path('catalog/brand/<int:pk>/tools/', cache_page(1)(BrandToolsListView.as_view()), name='brand_tool'),
 
 ]
