a63f477b955b43fdda2f11952eeeecbf95a79c1f
oleg425top <oleg59152@gmail.com>
2025-06-24 11:59:18 +0500

    Добавил url's для ToolUpdateView, BrandToolsListView и шаблон tool_update.html

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.views.decorators.cache import cache_page, never_cache
 
 from shop.apps import ShopConfig
-from shop.views import BrandListView, BrandCreateView, about_view, ToolListView, ToolCreateView, ToolDeleteView
+from shop.views import BrandListView, BrandCreateView, about_view, ToolListView, ToolCreateView, ToolDeleteView, \
+    ToolUpdateView, BrandToolsListView
 
 app_name = ShopConfig.name
 
@@ -14,5 +15,7 @@ urlpatterns = [
     path('catalog/tools/', cache_page(1)(ToolListView.as_view()), name='tools'),
     path('catalog/tools/create', cache_page(1)(ToolCreateView.as_view()), name='tool_create'),
     path('catalog/tools/delete/<int:pk>/', cache_page(1)(ToolDeleteView.as_view()), name='tool_delete'),
+    path('catalog/tools/update/<int:pk>/', cache_page(1)(ToolUpdateView.as_view()), name='tool_update'),
+    path('catalog/brand/<int:pk>/tools/', cache_page(1)(BrandToolsListView.as_view()), name='brand_tool'),
 
 ]
