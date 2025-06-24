64cf06eafc57d33e6b656cede52d64d9838c3d90
oleg425top <oleg59152@gmail.com>
2025-06-24 11:50:48 +0500

    Добавил BrandToolsListView,ToolUpdateView Добавил шаблон для отображения инструментов по брендам Добавил ссылку в карчточку инструмента

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 
-from shop.forms import BrandForms, ToolForms
+from shop.forms import BrandForms, ToolForms, ToolAdminForm
 from shop.models import Brand, Tool
 from users.models import UserRolls
 
@@ -43,6 +44,7 @@ class BrandCreateView(LoginRequiredMixin, CreateView):
         self.object.save()
         return super().form_valid(form)
 
+
 class ToolListView(ListView):
     model = Tool
     extra_context = {
@@ -50,6 +52,7 @@ class ToolListView(ListView):
     }
     template_name = 'shop/tool_list.html'
     paginate_by = 3
+
 
 class ToolCreateView(LoginRequiredMixin, CreateView):
     model = Tool
@@ -81,3 +84,46 @@ class ToolDeleteView(PermissionRequiredMixin, DeleteView):
         object_ = self.get_object()
         context_data['title'] = f'Удалить инструмент {object_}'
         return context_data
+
+
+class ToolUpdateView(LoginRequiredMixin, UpdateView):
+    model = Tool
+    form_class = ToolForms
+    template_name = 'shop/tool_update.html'
+    success_url = reverse_lazy('shop:tools')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        object_ = self.get_object()
+        print(object_)
+        context_data['title'] = f'Изменить инструмент {object_}'
+        return context_data
+
+    # def get_success_url(self):
+    #     return reverse('shop:tools', args=[self.kwargs.get('pk')])
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        # if self.object.owner != self.request.user and not self.request.user.is_staff:
+        #     raise Http404
+        return self.object
+
+    def get_form_class(self):
+        dog_forms = {
+            UserRolls.ADMIN: ToolAdminForm,
+            UserRolls.MODERATOR: ToolForms,
+            UserRolls.USER: ToolForms,
+        }
+        user_role = self.request.user.role
+        dog_form_class = dog_forms[user_role]
+        return dog_form_class
+
+
+class BrandToolsListView(LoginRequiredMixin, ListView):
+    model = Tool
+    template_name = 'shop/brand_tools.html'
+    extra_context = {'title': 'Инструменты данного брэнда'}
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(brand_id=self.kwargs.get('pk'))
+        return queryset
