8a1496bc47cf76664e034fd933db854f0600e66a
oleg425top <oleg59152@gmail.com>
2025-06-25 16:39:07 +0500

    Прописал методы UserCartView, cart_add в файл views.py приложения Cart

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
-def cart_add(request):
- pass
+from carts.models import Cart
+from shop.models import Tool
+
+
+class UserCartView(TemplateView):
+    template_name = 'carts/cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Корзина'
+        return context
+
+
+def cart_add(request, slug):
+    tool = Tool.objects.get(slug=slug)
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, tool=tool)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, tool=tool, quantity=1)
+
+    return redirect(request.META['HTTP_REFERER'])
