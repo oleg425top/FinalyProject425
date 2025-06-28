bd9e7a9dd6b93b3e36e5b19d16322389d819d80f
oleg425top <oleg59152@gmail.com>
2025-06-28 15:47:12 +0500

    Добавил verbose_name для пользователей

--- users/models.py
+++ users/models.py
@@ -14,10 +14,10 @@ class UserRolls(models.TextChoices):
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
-    role = models.CharField(max_length=9, choices=UserRolls.choices, default=UserRolls.USER)
+    role = models.CharField(max_length=9, choices=UserRolls.choices, default=UserRolls.USER, verbose_name='Статус')
     first_name = models.CharField(max_length=150, verbose_name='Имя', default='Аноним')
     phone = models.CharField(max_length=35, unique=True, verbose_name='phone_number', **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='active')
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
