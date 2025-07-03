9e9cda715b00708162ced87a27f538d90e864914
oleg425top <oleg59152@gmail.com>
2025-07-03 15:15:30 +0500

    Изменил форму регистрации заказов в Админке

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,69 @@ from django.contrib import admin
 
 from orders.models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+# admin.site.register(Order)
+# admin.site.register(OrderItem)
+
+class OrderItemTabularAdmin(admin.TabularInline):
+    model = OrderItem
+    fields = "tool", "name", "price", "quantity"
+    search_fields = (
+        "product",
+        "name",
+    )
+    extra = 0
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = "order", "tool", "name", "price", "quantity"
+    search_fields = (
+        "order",
+        "tool",
+        "name",
+    )
+
+
+class OrderTabularAdmin(admin.TabularInline):
+    model = Order
+    fields = (
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+
+    search_fields = (
+        "requires_delivery",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+    readonly_fields = ("created_timestamp",)
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+
+    search_fields = (
+        "id",
+    )
+    readonly_fields = ("created_timestamp",)
+    list_filter = (
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+    )
+    inlines = (OrderItemTabularAdmin,)
