a2d6109b680c4236606b5e51602c3ab5dd198284
oleg425top <oleg59152@gmail.com>
2025-07-06 17:18:30 +0500

    Отправка сообщения при оформлении заказа

--- users/services.py
+++ users/services.py
@@ -9,3 +9,12 @@ def send_register_email(email):
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email, ]
     )
+
+
+def send_order_email(email):
+    send_mail(
+        subject='Поздравляем c оформлением заказа',
+        message='Вы успешно оформили заказ на нашем сайте ',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email, ]
+    )
