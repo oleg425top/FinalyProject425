d014182f1aaa3be49aea0aaeb4aed16190aeecd3
oleg425top <oleg59152@gmail.com>
2025-07-08 12:16:07 +0500

    Исправил баг сброса фильтров при пагинации

--- shop/templatetags/my_tags.py
+++ shop/templatetags/my_tags.py
@@ -15,3 +15,11 @@ def tool_media(val):
     if val:
         return f'/media/{val}'
     return '/static/no_image.jpg'
+
+
+@register.simple_tag(takes_context=True)
+def query_transform(context, **kwargs):
+    query = context['request'].GET.copy()
+    for key, value in kwargs.items():
+        query[key] = value
+    return query.urlencode()
