7c90acd430f511cda95fb401186106a7c2f511c2
oleg425top <oleg59152@gmail.com>
2025-07-10 18:24:07 +0500

    Переписал методы на классы CartAddView, CartRemoveView, изменил соответственно и url's

--- carts/urls.py
+++ carts/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 
-from carts import views
 from carts.apps import CartsConfig
-from carts.views import UserCartView
+from carts.views import UserCartView, CartAddView, CartRemoveView
 
 """Модуль URL-маршрутов для приложения корзины."""
 app_name = CartsConfig.name
 
 urlpatterns = [
-    path('cart_add/<slug:slug>/', views.cart_add, name='cart_add'),
+    path('cart_add/<slug:slug>/', CartAddView.as_view(), name='cart_add'),
     path('user_cart/', UserCartView.as_view(), name='cart'),
     # path ('cart_change/<slug:slug>/', views.cart_change, name= 'cart_change'),
-    path('cart_remove/<int:id>/', views.cart_remove, name='cart_remove'),
+    path('cart_remove/<int:id>/', CartRemoveView.as_view(), name='cart_remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import TemplateView, View
 
 from carts.models import Cart
 from shop.models import Tool
@@ -16,36 +16,41 @@ class UserCartView(TemplateView):
         return context
 
 
-def cart_add(request, slug):
+class CartAddView(View):
     """
-        Добавляет товар в корзину пользователя.
+    Класс представления для добавления товара в корзину пользователя.
+    Если пользователь аутентифицирован, увеличивает количество товара в корзине.
+    Если товара нет в корзине, создает новую запись в корзине.
+    """
 
-        Если пользователь аутентифицирован, увеличивает количество товара в корзине.
-        Если товара нет в корзине, создает новую запись в корзине.
+    def get(self, request, slug):
+        tool = get_object_or_404(Tool, slug=slug)
 
-        Args:
-            request: Объект запроса.
-            slug: Slug товара для добавления в корзину.
+        if request.user.is_authenticated:
+            carts = Cart.objects.filter(user=request.user, tool=tool)
+            if carts.exists():
+                cart = carts.first()
+                if cart:
+                    cart.quantity += 1
+                    cart.save()
+            else:
+                Cart.objects.create(user=request.user, tool=tool, quantity=1)
 
-        Returns:
-            Перенаправляет на предыдущую страницу.
+        response_data = {
+            "message": "Товар добавлен в корзину",
+        }
+
+        return redirect(request.META.get('HTTP_REFERER', '/'), response_data)
+
+
+#
+class CartRemoveView(View):
+    """
+    Класс представления для удаления товара из корзины пользователя.
+    """
+
+    def get(self, request, id):
         """
-    tool = Tool.objects.get(slug=slug)
-    if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, tool=tool)
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
-        else:
-            Cart.objects.create(user=request.user, tool=tool, quantity=1)
-
-    return redirect(request.META['HTTP_REFERER'])
-
-
-def cart_remove(request, id):
-    """
         Удаляет товар из корзины пользователя.
 
         Args:
@@ -55,6 +60,6 @@ def cart_remove(request, id):
         Returns:
             Перенаправляет на предыдущую страницу.
         """
-    cart = Cart.objects.get(id=id)
-    cart.delete()
-    return redirect(request.META['HTTP_REFERER'])
+        cart = get_object_or_404(Cart, id=id)
+        cart.delete()
+        return redirect(request.META.get('HTTP_REFERER', '/'))
