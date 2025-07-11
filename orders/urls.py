76c4e93af1fa728a9fb026bc16734907ec17f8dc
oleg425top <oleg59152@gmail.com>
2025-07-11 11:31:02 +0500

    Переписал создание заказа на CBV , изменил url

--- orders/urls.py
+++ orders/urls.py
@@ -7,6 +7,5 @@ from orders.apps import OrdersConfig
 app_name = OrdersConfig.name
 
 urlpatterns = [
-    # path('create_order/', views.CreateOrderView.as_view(), name='create_order'),
-    path('create_order/', views.create_order, name='create_order'),
+    path('create_order/', views.CreateOrderView.as_view(), name='create_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
 from carts.models import Cart
 from orders.forms import CreateOrderForm
@@ -9,71 +12,67 @@ from orders.models import Order, OrderItem
 from users.services import send_order_email
 
 
-def create_order(request):
-    """
-    Обрабатывает создание заказа из корзины пользователя.
+class CreateOrderView(LoginRequiredMixin, FormView):
+    template_name = 'orders/create_order.html'
+    form_class = CreateOrderForm
+    success_url = reverse_lazy('users:profile')
 
-    Если метод запроса POST, валидирует форму и создает заказ.
-    В случае успешного создания заказа отправляет подтверждение на почту пользователя.
-    При ошибках валидации возвращает сообщение об ошибке.
-    Если метод запроса не POST, инициализирует форму с данными пользователя.
-    """
-    if request.method == 'POST':
-        form = CreateOrderForm(data=request.POST)
-        if form.is_valid():
-            try:
-                with transaction.atomic():
-                    user = request.user
-                    cart_items = Cart.objects.filter(user=user)
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['email'] = self.request.user.email
+        initial['phone_number'] = self.request.user.phone
+        return initial
 
-                    if cart_items.exists():
-                        # Создать заказ
-                        order = Order.objects.create(
-                            user=user,
-                            phone_number=form.cleaned_data['phone_number'],
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+                user = self.request.user
+                cart_items = Cart.objects.filter(user=user)
+
+                if cart_items.exists():
+                    # Создать заказ
+                    order = Order.objects.create(
+                        user=user,
+                        phone_number=form.cleaned_data['phone_number'],
+                        requires_delivery=form.cleaned_data['requires_delivery'],
+                        delivery_address=form.cleaned_data['delivery_address'],
+                        payment_on_get=form.cleaned_data['payment_on_get'],
+                    )
+                    # Создать заказанные товары
+                    for cart_item in cart_items:
+                        tool = cart_item.tool
+                        name = cart_item.tool.name
+                        price = cart_item.tool.sell_price()
+                        quantity = cart_item.quantity
+
+                        if tool.quantity < quantity:
+                            raise ValidationError(f'Недостаточное количество товара {name} на складе\
+                                                       В наличии - {tool.quantity}')
+
+                        OrderItem.objects.create(
+                            order=order,
+                            tool=tool,
+                            name=name,
+                            price=price,
+                            quantity=quantity,
                         )
-                        # Создать заказанные товары
-                        for cart_item in cart_items:
-                            tool = cart_item.tool
-                            name = cart_item.tool.name
-                            price = cart_item.tool.sell_price()
-                            quantity = cart_item.quantity
+                        tool.quantity -= quantity
+                        tool.save()
+                    cart_items.delete()
+                    messages.success(self.request, 'Заказ оформлен!')
+                    send_order_email(user.email)
+                    return redirect('users:profile')
+        except ValidationError as e:
+            messages.error(self.request, str(e))
+            return redirect('orders:create_order')
 
-                            if tool.quantity < quantity:
-                                raise ValidationError(f'Недостаточное количество товара {name} на складе\
-                                                           В наличии - {tool.quantity}')
+    def form_invalid(self, form):
+        messages.error(self.request, 'Заполните поля заново')
+        return redirect('orders:create_order')
 
-                            OrderItem.objects.create(
-                                order=order,
-                                tool=tool,
-                                name=name,
-                                price=price,
-                                quantity=quantity,
-                            )
-                            tool.quantity -= quantity
-                            tool.save()
-                        cart_items.delete()
-                        messages.success(request, 'Заказ оформлен!')
-                        send_order_email(user.email)
-                        return redirect('users:profile')
-            except ValidationError as e:
-                messages.error(request, str(e))
-                return redirect('orders:create_order')
-    else:
-        initial = {
-            'first_name': request.user.first_name,
-            'phone_number': request.user.phone,
-            'email': request.user.email,
-        }
-
-        form = CreateOrderForm(initial=initial)
-
-    context = {
-        'title': 'Home - Оформление заказа',
-        'form': form,
-        'order': True,
-    }
-    return render(request, 'orders/create_order.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Оформление заказа'
+        context['order'] = True
+        return context
