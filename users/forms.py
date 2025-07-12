c4ab8c1d7281a091e793e1ffda725d17e5f2cab5
oleg425top <oleg59152@gmail.com>
2025-07-12 18:15:05 +0500

    Добавил форму для привязки к шаблону

--- users/forms.py
+++ users/forms.py
@@ -62,3 +62,24 @@ class UserChangePasswordForm(StyleFormMixin, PasswordChangeForm):
             )
         password_validation.validate_password(password2, self.user)
         return password2
+
+
+class ContactsForm(forms.Form):
+    name = forms.CharField(
+        min_length=2,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Ваше имя:', 'class': 'form-control valid'}
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'placeholder': 'E-mail', 'class': 'form-control valid'}
+        )
+    )
+
+    message = forms.CharField(
+        min_length=10,
+        widget=forms.Textarea(
+            attrs={'placeholder': 'сообщение', 'cols': 30, 'rows': 9, 'class': 'form-control w-100'}
+        )
+    )
